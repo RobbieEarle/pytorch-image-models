58ca65128abcda77fd87065f212a6f7ebb84b071
rearle <rb504035@dal.ca>
2021-01-24 14:54:54 -0400

    Job: tl_caltech101
    Transfer learning experiment on Caltech-101 dataset.

--- sbatch_effnet_train.sh
+++ sbatch_effnet_train.sh
@@ -2,25 +2,23 @@
 #SBATCH -p t4v2
 #SBATCH --exclude=gpu102
 #SBATCH --exclude=gpu115
-#SBATCH --gres=gpu:8                        # request GPU(s)
+#SBATCH --gres=gpu:4                        # request GPU(s)
 #SBATCH --qos=high
 #SBATCH -c 32                                # number of CPU cores
 #SBATCH --mem=128G                           # memory per node
 #SBATCH --time=500:00:00                     # max walltime, hh:mm:ss
 #SBATCH --array=42%1                    # array value
-#SBATCH --output=logs_new/nef_partial/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=nef_partial
+#SBATCH --output=logs_new/tl_caltech101/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=tl_caltech101
 
 source ~/.bashrc
 source activate ~/venvs/efficientnet_train
 
 ACTFUN="$1"
-AMP="$2"
-MULT="$3"
-PARTIAL="$4"
+LR="$2"
 SEED="$SLURM_ARRAY_TASK_ID"
 
-SAVE_PATH=~/pytorch-image-models/outputs/nef_partial
+SAVE_PATH=~/pytorch-image-models/outputs/tl_caltech101
 CHECK_PATH="/checkpoint/$USER/${SLURM_JOB_ID}"
 IMGNET_PATH=/scratch/ssd001/datasets/imagenet/
 
@@ -45,9 +43,8 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-~/utilities/log_gpu_cpu_stats -l 0.5 -n -1 "${SAVE_PATH}/${SLURM_ARRAY_TASK_ID}_${SLURM_NODEID}_${SLURM_ARRAY_JOB_ID}_compute_usage.log"&
+~/utilities/log_gpu_cpu_stats 2000 0.5 -n -1 "${SAVE_PATH}/${SLURM_ARRAY_TASK_ID}_${SLURM_NODEID}_${SLURM_ARRAY_JOB_ID}_compute_usage.log"&
 export LOGGER_PID="$!"
 
-./distributed_train.sh 8 $IMGNET_PATH --model efficientnet_b0 -b 64 --actfun $ACTFUN --output $SAVE_PATH --check-path $CHECK_PATH --seed $SEED --sched step --epochs 450 --decay-epochs 2.4 --decay-rate .97 --opt rmsproptf --opt-eps .001 -j 4 --warmup-lr 1e-6 --weight-decay 1e-5 --drop 0.2 --drop-connect 0.2 --model-ema --model-ema-decay 0.9999 --aa original --remode pixel --reprob 0.2 --lr .032 --control-amp $AMP --extra-channel-mult $MULT --weight-init orthogonal --partial_ho_actfun $PARTIAL
-
+!./distributed_train.sh 4 caltech101 --model efficientnet_b0 -b 20 --actfun $ACTFUN --output $SAVE_PATH --check-path $CHECK_PATH --seed $SEED --sched onecycle --epochs 100 --weight-init orthogonal --tl --lr $LR --num-classes 101 --control-amp amp
 kill $LOGGER_PID
